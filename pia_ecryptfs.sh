e6acc7f0bc1d1c1f8f5b0215c28913921759cb57
GAS85 <GAS85@users.noreply.github.com>
2017-04-23 15:20:51 +0200

    Ubuntu 16.04 and PIA 66+ for ecryptfs

---
+++ pia_ecryptfs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$(whoami)" != "root" ]; then
+	echo "This script needs to be run as root. Try again with 'sudo $0'"
+	exit
+fi
+
+echo -n "Please enter your Linux username: "
+read linux_username
+
+mkdir /usr/local/bin/pia
+
+#it is already owned by root, but aynway...
+chown root:root /usr/local/bin/pia
+echo "Moving PIA to a new destination"
+mv /home/$linux_username/.pia_manager/ /usr/local/bin/pia/.pia_manager
+ln -s /usr/local/bin/pia/.pia_manager/ /home/'$linux_usermane'/.pia_manager
+
+echo "Done!"
+exit
