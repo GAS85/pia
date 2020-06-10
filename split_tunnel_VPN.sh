67d635c88d5932f2a0d2bbfa96529d4a8b9e759b
Georgiy Sitnikov <GAS85@users.noreply.github.com>
2020-06-10 13:37:05 +0200

    Correction
    #2 Variable needs to be hidden from the main script. File should be generated correctly now.

--- split_tunnel_VPN.sh
+++ split_tunnel_VPN.sh
@@ -174,40 +174,40 @@ cat > /etc/openvpn/iptables.sh << EOF
 
 export INTERFACE="tun0"
 export VPNUSER="vpn"
-export LOCALIP="'$localipaddr'"
-export NETIF="'$interface'"
+export LOCALIP="$localipaddr"
+export NETIF="$interface"
 
 # flushes all the iptables rules, if you have other rules to use then add them into the script
 iptables -F -t nat
 iptables -F -t mangle
 iptables -F -t filter
 
-# mark packets from $VPNUSER
+# mark packets from \$VPNUSER
 iptables -t mangle -A OUTPUT -j CONNMARK --restore-mark
-iptables -t mangle -A OUTPUT ! --dest $LOCALIP -m owner --uid-owner $VPNUSER -j MARK --set-mark 0x1
-iptables -t mangle -A OUTPUT --dest $LOCALIP -p udp --dport 53 -m owner --uid-owner $VPNUSER -j MARK --set-mark 0x1
-iptables -t mangle -A OUTPUT --dest $LOCALIP -p tcp --dport 53 -m owner --uid-owner $VPNUSER -j MARK --set-mark 0x1
+iptables -t mangle -A OUTPUT ! --dest \$LOCALIP -m owner --uid-owner \$VPNUSER -j MARK --set-mark 0x1
+iptables -t mangle -A OUTPUT --dest \$LOCALIP -p udp --dport 53 -m owner --uid-owner \$VPNUSER -j MARK --set-mark 0x1
+iptables -t mangle -A OUTPUT --dest \$LOCALIP -p tcp --dport 53 -m owner --uid-owner \$VPNUSER -j MARK --set-mark 0x1
 # Added Local Open Ports Like Aria2 RPC, Torrent GUI
-iptables -t mangle -A OUTPUT --src $LOCALIP -p tcp -m tcp -m multiport --sports 6800,7777 -m owner --uid-owner $VPNUSER -j MARK --set-mark 0x0
+iptables -t mangle -A OUTPUT --src \$LOCALIP -p tcp -m tcp -m multiport --sports 6800,7777 -m owner --uid-owner \$VPNUSER -j MARK --set-mark 0x0
 # Continue marking
-iptables -t mangle -A OUTPUT ! --src $LOCALIP -j MARK --set-mark 0x1
+iptables -t mangle -A OUTPUT ! --src \$LOCALIP -j MARK --set-mark 0x1
 iptables -t mangle -A OUTPUT -j CONNMARK --save-mark
 
 # allow responses
-iptables -A INPUT -i $INTERFACE -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -A INPUT -i \$INTERFACE -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
-# block everything incoming on $INTERFACE to prevent accidental exposing of ports
-iptables -A INPUT -i $INTERFACE -j REJECT
+# block everything incoming on \$INTERFACE to prevent accidental exposing of ports
+iptables -A INPUT -i \$INTERFACE -j REJECT
 
-# let $VPNUSER access lo and $INTERFACE
-iptables -A OUTPUT -o lo -m owner --uid-owner $VPNUSER -j ACCEPT
-iptables -A OUTPUT -o $INTERFACE -m owner --uid-owner $VPNUSER -j ACCEPT
+# let \$VPNUSER access lo and \$INTERFACE
+iptables -A OUTPUT -o lo -m owner --uid-owner \$VPNUSER -j ACCEPT
+iptables -A OUTPUT -o \$INTERFACE -m owner --uid-owner \$VPNUSER -j ACCEPT
 
-# all packets on $INTERFACE needs to be masqueraded
-iptables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE
+# all packets on \$INTERFACE needs to be masqueraded
+iptables -t nat -A POSTROUTING -o \$INTERFACE -j MASQUERADE
 
-# reject connections from predator IP going over $NETIF
-iptables -A OUTPUT ! --src $LOCALIP -o $NETIF -j REJECT
+# reject connections from predator IP going over \$NETIF
+iptables -A OUTPUT ! --src \$LOCALIP -o \$NETIF -j REJECT
 
 #ADD YOUR RULES HERE
 
