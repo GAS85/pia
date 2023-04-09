d11cdd163e15f5b6b86e193b20dbf2e6c020bddc
slycordinator <68940237+slycordinator@users.noreply.github.com>
2023-04-09 14:31:22 +0900

    cleanup of shell scripts
    * Changed `` notation to use modern $() notation
    * Added escapes to variables and other $ calls inside calls to cat
    
    * changed scripts to use /bin/sh as everything used is portable
    * changed whoami call to use id -u as id -u is more portable
    * changed ifconfig to use "ip addr show", as ifconfig has been deprecated/removed
    Also, in

--- pia_ecryptfs.sh
+++ pia_ecryptfs.sh
@@ -1,21 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ "$(whoami)" != "root" ]; then
+if [ "$(id -u)" -ne 0 ]; then
 	echo "This script needs to be run as root. Try again with 'sudo $0'"
 	exit
 fi
 
-echo -n "Please enter your Linux username: "
-read linux_username
-
+read -rp 'Please enter your Linux username: ' linux_username
+user_dir=/home/$linux_username
 mkdir /usr/local/bin/pia
 
 #it is already owned by root, but aynway...
 chown root:root /usr/local/bin/pia
 echo "Moving PIA to a new destination"
-mv /home/$linux_username/.pia_manager/ /usr/local/bin/pia/.pia_manager
+mv "$user_dir/.pia_manager" /usr/local/bin/pia/.pia_manager
 echo "Creating link"
-ln -s /usr/local/bin/pia/.pia_manager/ /home/$linux_usermane/.pia_manager
+ln -s /usr/local/bin/pia/.pia_manager/ "$user_dir/.pia_manager"
 
 echo "Done!"
 exit
